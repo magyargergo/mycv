fdebc9dbb85ec829a4108305184be8b9c4da48b7
Gergo Magyar <gergomagayr@icloud.com>
2024-10-17 08:41:58 +0100

    add speed insights to the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { ReactNode } from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: 'Gergo Magyar | Resume',
@@ -17,6 +18,7 @@ export default function RootLayout({
           <body>
             {children}
             <Analytics />
+            <SpeedInsights />
           </body>
       </html>
   );
